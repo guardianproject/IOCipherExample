cb3eb7b65c6971e78011c3b6558b766042ea1f89
Hans-Christoph Steiner <hans@eds.org>
2014-09-04 10:48:25 -0400

    format code using Eclipse's formatter in Android mode

--- src/info/guardianproject/iocipherexample/FileBrowser.java
+++ src/info/guardianproject/iocipherexample/FileBrowser.java
@@ -1,3 +1,4 @@
+
 package info.guardianproject.iocipherexample;
 
 import android.app.AlertDialog;
@@ -30,211 +31,216 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileBrowser extends ListActivity {
-	private final static String TAG = "FileBrowser";
+    private final static String TAG = "FileBrowser";
 
-	private List<String> item = null;
-	private List<String> path = null;
-	private TextView fileInfo;
-	private String[] items;
-	private String dbFile;
-	private String root = "/";
-	private VirtualFileSystem vfs;
+    private List<String> item = null;
+    private List<String> path = null;
+    private TextView fileInfo;
+    private String[] items;
+    private String dbFile;
+    private String root = "/";
+    private VirtualFileSystem vfs;
 
-	/** Called when the activity is first created. */
+    /** Called when the activity is first created. */
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		Intent intent = getIntent();
-		String action = intent.getAction();
-		String type = intent.getType();
-		if (Intent.ACTION_SEND.equals(action) && type != null) {
-			if (intent.hasExtra(Intent.EXTRA_STREAM)) {
-				Log.i(TAG, "save extra stream URI");
-				handleSendUri((Uri) intent.getExtras().get(Intent.EXTRA_STREAM));
-			} else {
-				Log.i(TAG, "save data");
-				handleSendUri(intent.getData());
-			}
-		}
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (intent.hasExtra(Intent.EXTRA_STREAM)) {
+                Log.i(TAG, "save extra stream URI");
+                handleSendUri((Uri) intent.getExtras().get(Intent.EXTRA_STREAM));
+            } else {
+                Log.i(TAG, "save data");
+                handleSendUri(intent.getData());
+            }
+        }
 
-		setContentView(R.layout.main);
-		fileInfo = (TextView) findViewById(R.id.info);
-		dbFile = getDir("vfs", MODE_PRIVATE).getAbsolutePath() + "/myfiles.db";
-	}
+        setContentView(R.layout.main);
+        fileInfo = (TextView) findViewById(R.id.info);
+        dbFile = getDir("vfs", MODE_PRIVATE).getAbsolutePath() + "/myfiles.db";
+    }
 
-	protected void onResume() {
-		super.onResume();
-		vfs = new VirtualFileSystem(dbFile);
-		// TODO don't use a hard-coded password! prompt for the password
-		vfs.mount("my fake password");
-		getFileList(root);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        vfs = new VirtualFileSystem(dbFile);
+        // TODO don't use a hard-coded password! prompt for the password
+        vfs.mount("my fake password");
+        getFileList(root);
+    }
 
-	protected void onDestroy() {
-		super.onDestroy();
-		vfs.unmount();
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        vfs.unmount();
+    }
 
-	private void handleSendUri(Uri dataUri) {
-		try {
-			ContentResolver cr = getContentResolver();
-			InputStream in = cr.openInputStream(dataUri);
-			Log.i(TAG, "incoming URI: " + dataUri.toString());
-			String fileName = dataUri.getLastPathSegment();
-			File f = new File("/" + fileName);
-			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(f));
-			readBytesAndClose(in, out);
-			Log.v(TAG, f.getAbsolutePath() + " size: " + String.valueOf(f.length()));
-		} catch (Exception e) {
-			Log.e(TAG, e.getMessage(), e);
-		}
-	}
+    private void handleSendUri(Uri dataUri) {
+        try {
+            ContentResolver cr = getContentResolver();
+            InputStream in = cr.openInputStream(dataUri);
+            Log.i(TAG, "incoming URI: " + dataUri.toString());
+            String fileName = dataUri.getLastPathSegment();
+            File f = new File("/" + fileName);
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(f));
+            readBytesAndClose(in, out);
+            Log.v(TAG, f.getAbsolutePath() + " size: " + String.valueOf(f.length()));
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
 
-	private void readBytesAndClose(InputStream in, OutputStream out)
-			throws IOException {
-		try {
-			int block = 8 * 1024; // IOCipher works best with 8k blocks
-			byte[] buff = new byte[block];
-			while (true) {
-				int len = in.read(buff, 0, block);
-				if (len < 0) {
-					break;
-				}
-				out.write(buff, 0, len);
-			}
-		} finally {
-			in.close();
-			out.flush();
-			out.close();
-		}
-	}
+    private void readBytesAndClose(InputStream in, OutputStream out)
+            throws IOException {
+        try {
+            int block = 8 * 1024; // IOCipher works best with 8k blocks
+            byte[] buff = new byte[block];
+            while (true) {
+                int len = in.read(buff, 0, block);
+                if (len < 0) {
+                    break;
+                }
+                out.write(buff, 0, len);
+            }
+        } finally {
+            in.close();
+            out.flush();
+            out.close();
+        }
+    }
 
-	// To make listview for the list of file
-	public void getFileList(String dirPath) {
+    // To make listview for the list of file
+    public void getFileList(String dirPath) {
 
-		item = new ArrayList<String>();
-		path = new ArrayList<String>();
+        item = new ArrayList<String>();
+        path = new ArrayList<String>();
 
-		File file = new File(dirPath);
-		File[] files = file.listFiles();
+        File file = new File(dirPath);
+        File[] files = file.listFiles();
 
-		if (!dirPath.equals(root)) {
-			item.add(root);
-			path.add(root);// to get back to main list
+        if (!dirPath.equals(root)) {
+            item.add(root);
+            path.add(root);// to get back to main list
 
-			item.add("..");
-			path.add(file.getParent()); // back one level
-		}
+            item.add("..");
+            path.add(file.getParent()); // back one level
+        }
 
-		for (int i = 0; i < files.length; i++) {
+        for (int i = 0; i < files.length; i++) {
 
-			File fileItem = files[i];
-			path.add(fileItem.getPath());
-			if (fileItem.isDirectory()) {
-				// input name directory to array list
-				item.add("[" + fileItem.getName() + "]");
-			} else {
-				// input name file to array list
-				item.add(fileItem.getName());
-			}
-		}
-		fileInfo.setText("Info: " + dirPath + " [ " + files.length + " item ]");
-		// declare array with specific number of items
-		items = new String[item.size()];
-		// send data arraylist(item) to array(items)
-		item.toArray(items);
-		setListAdapter(new IconicList());
-	}
+            File fileItem = files[i];
+            path.add(fileItem.getPath());
+            if (fileItem.isDirectory()) {
+                // input name directory to array list
+                item.add("[" + fileItem.getName() + "]");
+            } else {
+                // input name file to array list
+                item.add(fileItem.getName());
+            }
+        }
+        fileInfo.setText("Info: " + dirPath + " [ " + files.length + " item ]");
+        // declare array with specific number of items
+        items = new String[item.size()];
+        // send data arraylist(item) to array(items)
+        item.toArray(items);
+        setListAdapter(new IconicList());
+    }
 
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		File file = new File(path.get(position));
-		if (file.isDirectory()) {
-			if (file.canRead()) {
-				getFileList(path.get(position));
-			} else {
-				new AlertDialog.Builder(this)
-						.setIcon(R.drawable.icon)
-						.setTitle(
-								"[" + file.getName() + "] folder can't be read")
-						.setPositiveButton("OK",
-								new DialogInterface.OnClickListener() {
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        File file = new File(path.get(position));
+        if (file.isDirectory()) {
+            if (file.canRead()) {
+                getFileList(path.get(position));
+            } else {
+                new AlertDialog.Builder(this)
+                        .setIcon(R.drawable.icon)
+                        .setTitle(
+                                "[" + file.getName() + "] folder can't be read")
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
 
-									// @Override
-									public void onClick(DialogInterface dialog,
-											int which) {
-										// TODO Auto-generated method stub
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                            int which) {
+                                        // TODO Auto-generated method stub
 
-									}
-								}).show();
+                                    }
+                                }).show();
 
-			}
-		} else {
-			Log.i(TAG,"open URL: " + Uri.parse(IOCipherContentProvider.FILES_URI + file.getName()));
-			final Uri uri = Uri.parse(IOCipherContentProvider.FILES_URI + file.getName());
-			new AlertDialog.Builder(this)
-					.setIcon(R.drawable.icon)
-					.setTitle("[" + file.getName() + "]")
-					.setNeutralButton("View",
-							new DialogInterface.OnClickListener() {
-						// @Override
-						public void onClick(DialogInterface dialog,
-								int which) {
-							try {
-								startActivity(new Intent(Intent.ACTION_VIEW, uri));
-							} catch (ActivityNotFoundException e) {
-								Log.e(TAG, "No relevant Activity found", e);
-							}
-						}
-					})
-					.setPositiveButton("Share...",
-							new DialogInterface.OnClickListener() {
+            }
+        } else {
+            Log.i(TAG, "open URL: " + Uri.parse(IOCipherContentProvider.FILES_URI + file.getName()));
+            final Uri uri = Uri.parse(IOCipherContentProvider.FILES_URI + file.getName());
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.icon)
+                    .setTitle("[" + file.getName() + "]")
+                    .setNeutralButton("View",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                        int which) {
+                                    try {
+                                        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                                    } catch (ActivityNotFoundException e) {
+                                        Log.e(TAG, "No relevant Activity found", e);
+                                    }
+                                }
+                            })
+                    .setPositiveButton("Share...",
+                            new DialogInterface.OnClickListener() {
 
-								// @Override
-								public void onClick(DialogInterface dialog,
-										int which) {
-									Intent intent = new Intent(Intent.ACTION_SEND, uri);
-									String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
-									String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension);
-									if (mimeType == null)
-										mimeType = "application/octet-stream";
-									Log.i(TAG, "mime type: "+ mimeType);
-									intent.setType(mimeType);
-									try {
-										startActivity(Intent.createChooser(intent, "Share this!"));
-									} catch (ActivityNotFoundException e) {
-										Log.e(TAG, "No relevant Activity found", e);
-									}
-								}
-							}).show();
-		}
-	}
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                        int which) {
+                                    Intent intent = new Intent(Intent.ACTION_SEND, uri);
+                                    String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri
+                                            .toString());
+                                    String mimeType = MimeTypeMap.getSingleton()
+                                            .getMimeTypeFromExtension(fileExtension);
+                                    if (mimeType == null)
+                                        mimeType = "application/octet-stream";
+                                    Log.i(TAG, "mime type: " + mimeType);
+                                    intent.setType(mimeType);
+                                    try {
+                                        startActivity(Intent.createChooser(intent, "Share this!"));
+                                    } catch (ActivityNotFoundException e) {
+                                        Log.e(TAG, "No relevant Activity found", e);
+                                    }
+                                }
+                            }).show();
+        }
+    }
 
-	class IconicList extends ArrayAdapter {
+    class IconicList extends ArrayAdapter {
 
-		public IconicList() {
-			super(FileBrowser.this, R.layout.row, items);
+        public IconicList() {
+            super(FileBrowser.this, R.layout.row, items);
 
-			// TODO Auto-generated constructor stub
-		}
+            // TODO Auto-generated constructor stub
+        }
 
-		public View getView(int position, View convertView, ViewGroup parent) {
-			LayoutInflater inflater = getLayoutInflater();
-			View row = inflater.inflate(R.layout.row, null);
-			TextView label = (TextView) row.findViewById(R.id.label);
-			ImageView icon = (ImageView) row.findViewById(R.id.icon);
-			label.setText(items[position]);
-			File f = new File(path.get(position)); // get the file according the
-													// position
-			if (f.isDirectory()) {
-				icon.setImageResource(R.drawable.folder);
-			} else {
-				icon.setImageResource(R.drawable.text);
-			}
-			return (row);
-		}
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View row = inflater.inflate(R.layout.row, null);
+            TextView label = (TextView) row.findViewById(R.id.label);
+            ImageView icon = (ImageView) row.findViewById(R.id.icon);
+            label.setText(items[position]);
+            File f = new File(path.get(position)); // get the file according the
+                                                   // position
+            if (f.isDirectory()) {
+                icon.setImageResource(R.drawable.folder);
+            } else {
+                icon.setImageResource(R.drawable.text);
+            }
+            return (row);
+        }
 
-	}
+    }
 }
--- src/info/guardianproject/iocipherexample/IOCipherContentProvider.java
+++ src/info/guardianproject/iocipherexample/IOCipherContentProvider.java
@@ -1,3 +1,4 @@
+
 package info.guardianproject.iocipherexample;
 
 // inspired by https://github.com/commonsguy/cw-omnibus/tree/master/ContentProvider/Pipe
@@ -21,90 +22,90 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class IOCipherContentProvider extends ContentProvider {
-	public static final String TAG = "IOCipherContentProvider";
-	public static final Uri FILES_URI = Uri
-			.parse("content://info.guardianproject.iocipherexample/");
-	private MimeTypeMap mimeTypeMap;
+    public static final String TAG = "IOCipherContentProvider";
+    public static final Uri FILES_URI = Uri
+            .parse("content://info.guardianproject.iocipherexample/");
+    private MimeTypeMap mimeTypeMap;
 
-	@Override
-	public boolean onCreate() {
-		mimeTypeMap = MimeTypeMap.getSingleton();
-		return true;
-	}
+    @Override
+    public boolean onCreate() {
+        mimeTypeMap = MimeTypeMap.getSingleton();
+        return true;
+    }
 
-	@Override
-	public String getType(Uri uri) {
-		String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
-		return mimeTypeMap.getMimeTypeFromExtension(fileExtension);
-	}
+    @Override
+    public String getType(Uri uri) {
+        String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+        return mimeTypeMap.getMimeTypeFromExtension(fileExtension);
+    }
 
-	@Override
-	public ParcelFileDescriptor openFile(Uri uri, String mode)
-			throws FileNotFoundException {
-		ParcelFileDescriptor[] pipe = null;
-		BufferedInputStream in = null;
+    @Override
+    public ParcelFileDescriptor openFile(Uri uri, String mode)
+            throws FileNotFoundException {
+        ParcelFileDescriptor[] pipe = null;
+        BufferedInputStream in = null;
 
-		try {
-			pipe = ParcelFileDescriptor.createPipe();
-			in = new BufferedInputStream(new FileInputStream(new File("/test.pdf")));
-			new PipeFeederThread(in,
-					new AutoCloseOutputStream(pipe[1])).start();
-		} catch (IOException e) {
-			Log.e(TAG, "Error opening pipe", e);
-			throw new FileNotFoundException("Could not open pipe for: "
-					+ uri.toString());
-		}
+        try {
+            pipe = ParcelFileDescriptor.createPipe();
+            in = new BufferedInputStream(new FileInputStream(new File("/test.pdf")));
+            new PipeFeederThread(in,
+                    new AutoCloseOutputStream(pipe[1])).start();
+        } catch (IOException e) {
+            Log.e(TAG, "Error opening pipe", e);
+            throw new FileNotFoundException("Could not open pipe for: "
+                    + uri.toString());
+        }
 
-		return (pipe[0]);
-	}
+        return (pipe[0]);
+    }
 
-	@Override
-	public Cursor query(Uri url, String[] projection, String selection,
-			String[] selectionArgs, String sort) {
-		throw new RuntimeException("Operation not supported");
-	}
+    @Override
+    public Cursor query(Uri url, String[] projection, String selection,
+            String[] selectionArgs, String sort) {
+        throw new RuntimeException("Operation not supported");
+    }
 
-	@Override
-	public Uri insert(Uri uri, ContentValues initialValues) {
-		throw new RuntimeException("Operation not supported");
-	}
+    @Override
+    public Uri insert(Uri uri, ContentValues initialValues) {
+        throw new RuntimeException("Operation not supported");
+    }
 
-	@Override
-	public int update(Uri uri, ContentValues values, String where,
-			String[] whereArgs) {
-		throw new RuntimeException("Operation not supported");
-	}
+    @Override
+    public int update(Uri uri, ContentValues values, String where,
+            String[] whereArgs) {
+        throw new RuntimeException("Operation not supported");
+    }
 
-	@Override
-	public int delete(Uri uri, String where, String[] whereArgs) {
-		throw new RuntimeException("Operation not supported");
-	}
+    @Override
+    public int delete(Uri uri, String where, String[] whereArgs) {
+        throw new RuntimeException("Operation not supported");
+    }
 
-	static class PipeFeederThread extends Thread {
-		InputStream in;
-		OutputStream out;
+    static class PipeFeederThread extends Thread {
+        InputStream in;
+        OutputStream out;
 
-		PipeFeederThread(InputStream in, OutputStream out) {
-			this.in = in;
-			this.out = out;
-		}
+        PipeFeederThread(InputStream in, OutputStream out) {
+            this.in = in;
+            this.out = out;
+        }
 
-		@Override
-		public void run() {
-			byte[] buf = new byte[8192];
-			int len;
+        @Override
+        public void run() {
+            byte[] buf = new byte[8192];
+            int len;
 
-			try {
-				while ((len = in.read(buf)) > 0) {
-					out.write(buf, 0, len);
-				}
+            try {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
 
-				in.close();
-				out.flush();
-				out.close();
-			} catch (IOException e) {
-				Log.e(TAG, "File transfer failed:", e);
-			}
-		}
-	}
+                in.close();
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                Log.e(TAG, "File transfer failed:", e);
+            }
+        }
+    }
 }
